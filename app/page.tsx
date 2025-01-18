67a71be5e248dbb5e94452196b9bea70ec096564
kcirreb <koberrick11@gmail.com>
2025-01-18 17:55:56 +0800

    feat: add page header

--- app/page.tsx
+++ app/page.tsx
@@ -3,17 +3,68 @@
 import Editor from "@/components/editor";
 import Wheel from "@/components/wheel";
 import { DEFAULT_OPTIONS } from "@/lib/constants";
-import { Group } from "@mantine/core";
+import {
+  ActionIcon,
+  Group,
+  Stack,
+  Text,
+  useComputedColorScheme,
+  useMantineColorScheme,
+} from "@mantine/core";
+import { IconBrandGithubFilled, IconMoon, IconSun } from "@tabler/icons-react";
 import { useState } from "react";
 
 export default function Home() {
   const [options, setOptions] = useState<string[]>(DEFAULT_OPTIONS);
   const [spinning, setSpinning] = useState<boolean>(false);
 
+  const { setColorScheme } = useMantineColorScheme();
+  const computedColorScheme = useComputedColorScheme("light");
+
+  const toggleColorScheme = () => {
+    setColorScheme(computedColorScheme === "light" ? "dark" : "light");
+  };
+
   return (
-    <Group justify="center" gap="xl" className="h-screen">
-      <Wheel options={options} spinning={spinning} setSpinning={setSpinning} />
-      <Editor options={options} setOptions={setOptions} spinning={spinning} />
-    </Group>
+    <Stack p="md" className="h-screen">
+      <Group justify="space-between">
+        <Text size="lg" fw={500}>
+          🎡 Decide for Me
+        </Text>
+        <Group gap="xs">
+          <ActionIcon
+            component="a"
+            href="https://github.com/kcirreb/decide-for-me"
+            target="_blank"
+            variant="default"
+            size="lg"
+            radius="md"
+          >
+            <IconBrandGithubFilled size={22} />
+          </ActionIcon>
+          <ActionIcon
+            onClick={toggleColorScheme}
+            variant="default"
+            size="lg"
+            radius="md"
+          >
+            {computedColorScheme === "light" ? (
+              <IconMoon size={22} stroke={1.5} />
+            ) : (
+              <IconSun size={22} stroke={1.5} />
+            )}
+          </ActionIcon>
+        </Group>
+      </Group>
+
+      <Group justify="center" gap="xl" h="100%">
+        <Wheel
+          options={options}
+          spinning={spinning}
+          setSpinning={setSpinning}
+        />
+        <Editor options={options} setOptions={setOptions} spinning={spinning} />
+      </Group>
+    </Stack>
   );
 }
